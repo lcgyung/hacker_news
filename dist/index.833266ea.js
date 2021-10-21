66689d97f59da2662364f297c147205150423293
lcgyung <lcgyung88@naver.com>
2021-10-22 00:59:45 +0900

    Added router

--- dist/index.833266ea.js
+++ dist/index.833266ea.js
@@ -464,37 +464,48 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+// API getData
 function getData(url) {
     ajax.open("GET", url, false);
     ajax.send();
     return JSON.parse(ajax?.response);
 }
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
-// hashchange 이벤트
-window.addEventListener("hashchange", ()=>{
+// 리스트 페이지
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
+    newsList.push("<ul>");
+    for(let i = 0; i < newsFeed.length; i++){
+        const o = newsFeed[i];
+        newsList.push(`
+  <li>
+    <a href="#${o?.id}">
+      ${o?.title} (${o?.comments_count})
+    </a>
+  </li>
+`);
+    }
+    newsList.push("</ul>");
+    container.innerHTML = newsList.join("");
+}
+// 상세 페이지
+function newsDetail() {
     const id = location?.hash.substr(1);
     const newsContent = getData(CONTENT_URL.replace("@id", id));
-    const title = document.createElement("h1");
-    title.innerHTML = newsContent?.title;
-    content.appendChild(title);
-});
-// 리스트 생성
-newsFeed.map((o)=>{
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <li>
-      <a href="#${o?.id}">
-        ${o?.title} (${o?.comments_count})
-      </a>
-    </li>
-  `;
-    // ul.appendChild(div.children[0]);
-    ul.appendChild(div?.firstElementChild);
-});
-// 렌더링
-container.appendChild(ul);
-container.appendChild(content);
+    container.innerHTML = `
+      <h1>${newsContent?.title}</h1>
+      <div>
+        <a href="#">목록으로</a>
+      </div>
+    `;
+}
+// 라우터
+function router() {
+    const routePath = location.hash;
+    routePath === "" ? newsFeed() : newsDetail();
+}
+window.addEventListener("hashchange", router);
+router();
 
 },{}]},["JZ7gt","lzYRN"], "lzYRN", "parcelRequirec54b")
 
